c341a3840a1d26a04cb308737f76c22d80d47fe3
Jorge <jfloresq1987@gmail.com>
2022-06-08 22:26:11 -0500

    docker file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Link } from './models/link'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-votos';
+
+  links:Link[];
+
+  constructor(){
+    this.links=[
+      new Link('Google','www.google.com',100),
+      new Link('Youtube','www.youtube.com',50),
+      new Link('Facebook','www.facebook.com',90)
+    ];
+  }
+
+  addLink(title:HTMLInputElement,link:HTMLInputElement){
+    this.links.push(new Link(title.value,link.value));
+    title.value='';
+    link.value='';
+    return false;
+  }
 }
